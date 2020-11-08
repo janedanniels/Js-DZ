c973a1703bb35b880a6ea1cea228ab53ebda0cf9
Jane <jane@jane>
2020-11-08 18:40:09 +0500

    DZ JS

---
+++ script.js
@@ -0,0 +1,23 @@
+
+var name = prompt('Введите своё имя')
+console.log(name)
+
+var age = prompt('Введите свой возраст')
+console.log('Мне ' + age)
+
+var first = prompt('Решите пример: 10 + 5 = ?')
+console.log('Пример 1: 10 + 5 = ' + first + ' Ваш ответ 15')
+
+var first = prompt('Решите пример: 20 - 15 = ?')
+console.log('Пример 2: 20 - 15 = ' + first + ' Ваш ответ 5')
+
+var first = prompt('Решите пример: 3 * 2 = ?')
+console.log('Пример 3: 3 * 2 = ' + first + ' Ваш ответ 6')
+
+var first = prompt('Решите пример: 20 / 2 = ?')
+console.log('Пример 4: 20 / 2 = ' + first + ' Ваш ответ 10')
+
+var first = prompt('Решите пример: 10 % 3= ?')
+console.log('Пример 4: 10 % 3 = ' + first + ' Ваш ответ 1')
+
+alert('Откройте консоль для просмотра!')
